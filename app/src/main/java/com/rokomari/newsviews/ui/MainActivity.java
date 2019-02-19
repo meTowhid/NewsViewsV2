4fd8b2a1b9db3fa9468c9d0492b69c3e02821552
Towhid <bt.towhid@gmail.com>
2019-02-19 12:18:04 +0600

    pull to refresh added

--- app/src/main/java/com/rokomari/newsviews/ui/MainActivity.java
+++ app/src/main/java/com/rokomari/newsviews/ui/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -22,27 +23,40 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.recycler_view)
     RecyclerView recycler;
 
+    @BindView(R.id.swipe_refresh)
+    SwipeRefreshLayout swipeRefresh;
+
+    private RecyclerViewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, new ArrayList<>());
+        adapter = new RecyclerViewAdapter(this, new ArrayList<>());
         recycler.setLayoutManager(new LinearLayoutManager(this));
         recycler.setHasFixedSize(true);
         recycler.setAdapter(adapter);
 
+        swipeRefresh.setOnRefreshListener(this::refreshData);
+        swipeRefresh.setRefreshing(true);
+        refreshData();
+    }
+
+    private void refreshData() {
         RemoteRepo.getInstance().getNewsArticles(new ResponseCallback() {
             @Override
             public void onSuccess(Object data) {
                 adapter.data = (List<Article>) data;
                 adapter.notifyDataSetChanged();
+                swipeRefresh.setRefreshing(false);
             }
 
             @Override
             public void onError(Throwable th) {
                 Toast.makeText(MainActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                swipeRefresh.setRefreshing(false);
             }
         });
     }
